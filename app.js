b62f53b17df7cb6a05aff95dc73b850995981302
Kaňkovský Adam <kankovsky@SSPTAJI.CZ>
2024-09-30 12:35:30 +0200

    First app

--- app.js
+++ app.js
@@ -10,6 +10,29 @@ if ('serviceWorker' in navigator) {
     });
 }
  
-function calcutalte() {
-    document.getElementById('hello').innerText = 'Hello world';
+function calculate() {
+    const first_number = parseFloat(document.getElementById('first_number').value);
+    const second_number = parseFloat(document.getElementById('second_number').value);
+    const operand_select = document.getElementById("operands");
+    const selected_operand = operand_select.options[operand_select.selectedIndex].text;
+
+    let res;
+
+    if (selected_operand == "+") {
+        res = first_number + second_number;
+    } else if (selected_operand == "-") {
+        res = first_number - second_number;
+    } else if (selected_operand == "*") {
+        res = first_number * second_number;
+    } else if (selected_operand == "/") {
+        if (second_number == 0) {
+            res = "You can't divide by 0";
+        } else {
+            res = first_number / second_number;
+        }
+    } else {
+        res = "Wrong operand selected";
+    }
+
+    document.getElementById('result').innerText = "Result: " + res;
 }
